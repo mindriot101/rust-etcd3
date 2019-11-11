554b4ad918b7df2768cb4b9cb76527abbfb482d1
Simon Walker <s.r.walker101@googlemail.com>
2019-11-11 15:30:28 +0000

    Include protobufs in main code
    Now the code builds the protobuf files into rust code, and includes the
    sources in the main `lib.rs`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+// Internal names, which are unfortunately named.
+mod mvccpb {
+    // Proto file: kv.proto
+    tonic::include_proto!("mvccpb");
+}
+
+mod authpb {
+    // Proto file: auth.proto
+    tonic::include_proto!("authpb");
+}
+
+mod etcdserver {
+    // Proto file: rpc.proto
+    tonic::include_proto!("etcdserverpb");
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
